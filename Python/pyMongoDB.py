3ed865249ad525b6d470521f48cc4ba79333119e
zidane501 <lau82@outlook.dk>
2022-02-04 17:28:49 +0100

    Removing API keys

--- Python/pyMongoDB.py
+++ Python/pyMongoDB.py
@@ -1,10 +1,15 @@
-from pymongo import MongoClient
+from pymongo import MongoClient,os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGODB_PW = os.getenv('MONGODB_PW')
 # print("hi")
 # pprint library is used to make the output look more pretty
 from pprint import pprint
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 def addEntrysToMongoDB(item)
-    client = MongoClient("mongodb+srv://sunny:qD7hsawC9gNCur5W@cluster0.ud157.mongodb.net/NearestButikker?retryWrites=true&w=majority") # 
+    client = MongoClient("mongodb+srv://sunny:{MONGODB_PW}@cluster0.ud157.mongodb.net/NearestButikker?retryWrites=true&w=majority") # 
     # db = client.test
 
     dbname=client["NearestButikker"]
